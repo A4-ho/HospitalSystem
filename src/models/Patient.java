001af0763e365b53a9f30f746bb95b981b831cd6
k1ays <tasimov82@bk.ru>
2025-01-29 11:32:55 +0500

    Update Patient.java

--- src/models/Patient.java
+++ src/models/Patient.java
@@ -3,26 +3,31 @@ package src.models;
 public class Patient {
     private int id;
     private String name;
+    private String surname;
     private int age;
     private String gender;
-    private String phone;
+    private String email;
 
-    public Patient(int id, String name, int age, String gender, String phone) {
+    public Patient(int id, String name, int age, String gender, String email) {
         this.id = id;
         this.name = name;
+        this.surname = surname; 
         this.age = age;
         this.gender = gender;
-        this.phone = phone;
+        this.email = email;
     }
 
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
+    public String getSurname() {return surname; }
+    public void setSurname(String surname) {this.surname = surname;} 
     public int getAge() { return age; }
     public void setAge(int age) { this.age = age; }
     public String getGender() { return gender; }
     public void setGender(String gender) { this.gender = gender; }
-    public String getPhone() { return phone; }
-    public void setPhone(String phone) { this.phone = phone; }
+    public String getEmail() { return email; }
+    public void setEmail() { this.email = email; } 
+   
 }
