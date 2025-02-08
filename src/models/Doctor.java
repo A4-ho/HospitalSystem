3db7c15fa573d6e771362e390a80138f133d4292
k1ays <tasimov82@bk.ru>
2025-02-08 13:07:16 +0500

    Controller added

--- src/models/Doctor.java
+++ src/models/Doctor.java
@@ -2,21 +2,34 @@ package src.models;
 
 public class Doctor extends User {
     private String specialization;
+
+    // Конструктор с вызовом родительского конструктора
+    public Doctor(int id, String name, String surname, String email, String password, String role, String specialization) {
+        super(id, name, surname, email, password, role);
         this.specialization = specialization;
     }
 
-    // Getter for specialization
+    // Геттер
     public String getSpecialization() {
         return specialization;
     }
 
-    // Setter for specialization (if needed)
+    // Сеттер
     public void setSpecialization(String specialization) {
-public class Doctor {
-    private int id;
-    private String name;
-        this.id = id;
-        this.name = name;
         this.specialization = specialization;
+    }
+
+    // Переопределяем toString
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "id=" + getId() +
+                ", name='" + getName() + '\'' +
+                ", surname='" + getSurname() + '\'' +
+                ", email='" + getEmail() + '\'' +
+                ", password='" + getPassword() + '\'' +
+                ", role='" + getRole() + '\'' +
+                ", specialization='" + specialization + '\'' +
+                '}';
     }
 }
